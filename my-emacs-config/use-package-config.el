3eec86b7260d86b779071fbd6e35a2ae22342005
Reuben Cornel <reuben.cornel@gmail.com>
2018-07-31 08:30:44 -0700

    Adding a keybinding for occur

--- my-emacs-config/use-package-config.el
+++ my-emacs-config/use-package-config.el
@@ -25,6 +25,7 @@
      helm-imenu-fuzzy-match    t)
     (global-set-key (kbd "C-c h") 'helm-command-prefix)
     (global-set-key (kbd "M-x") 'helm-M-x)
+    (global-set-key (kbd "M-s o") 'helm-occur)
     (global-set-key (kbd "C-x C-f") 'helm-find-files)
     (global-set-key (kbd "C-x b") 'helm-mini)
     (global-unset-key (kbd "C-x c"))   
