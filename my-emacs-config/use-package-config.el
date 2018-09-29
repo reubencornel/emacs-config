aa2bc21da5a7f80a7d1326b5c452027403a0181b
Reuben Cornel <reuben.cornel@gmail.com>
2018-09-28 21:53:44 -0700

    Adding scala mode config.

--- my-emacs-config/use-package-config.el
+++ my-emacs-config/use-package-config.el
@@ -81,4 +81,20 @@
   (eshell-git-prompt-use-theme 'robbyrussell))
 
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; RUST CONFIG ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; SCALA CONFIG ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(use-package ensime
+  :ensure t
+  :pin melpa)
+
+(use-package sbt-mode
+  :pin melpa)
+
+(use-package scala-mode
+  :pin melpa
+  :interpreter
+  ("scala" . scala-mode)
+  ("sc" . scala-mode))
+
+
+
