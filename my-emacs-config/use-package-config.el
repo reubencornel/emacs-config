b05d3c9b50c7e8d359a65a06873c2512ec46a624
Reuben Cornel <reuben.cornel@gmail.com>
2022-02-06 22:28:25 -0800

    Adding java lsp to emacs.

--- my-emacs-config/use-package-config.el
+++ my-emacs-config/use-package-config.el
@@ -1155,5 +1155,26 @@
  (setq lsp-haskell-process-path-hie "~/.ghcup/bin/haskell-language-server-wrapper")
  (setq lsp-haskell-process-args-hie '()) )
 
+(use-package lsp-java 
+  :ensure t
+  :config (add-hook 'java-mode-hook 'lsp))
+(use-package dap-mode
+  :ensure t
+  :after (lsp-mode)
+  :functions dap-hydra/nil
+  :config
+  (require 'dap-java)
+  :bind (:map lsp-mode-map
+         ("<f5>" . dap-debug)
+         ("M-<f5>" . dap-hydra))
+  :hook ((dap-mode . dap-ui-mode)
+    (dap-session-created . (lambda (&_rest) (dap-hydra)))
+    (dap-terminated . (lambda (&_rest) (dap-hydra/nil)))))
+(use-package helm-lsp
+:ensure t
+:after (lsp-mode)
+:commands (helm-lsp-workspace-symbol)
+:init (define-key lsp-mode-map [remap xref-find-apropos] #'helm-lsp-workspace-symbol))
+
 (provide 'use-package-config)
 ;;; use-package-config.el
