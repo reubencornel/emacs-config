614affebf767d1d0cbe86b8ee58e1f753a3756c8
Reuben Cornel <reuben.cornel@gmail.com>
2024-10-02 17:57:40 -0700

    Fixing the keybindings.

--- my-emacs-config/my-key-bindings.el
+++ my-emacs-config/my-key-bindings.el
@@ -6,49 +6,14 @@
 
 
 (global-unset-key [(f1)])
-(global-set-key [(f1)] 'next-multiframe-window)
+(global-set-key [(f1)] 'ace-window)
 
-(global-unset-key [(f2)])
-(defun open-daily-tasks()
-  (interactive)
-  (org-agenda "" "d"))
-(global-set-key [(f2)] 'org-agenda-list)
-
-(global-unset-key [(f3)])
-(defun open-inbox-today-entries()
-  (interactive)
-  (org-agenda "" "t"))
-(global-set-key [(f3)] 'open-inbox-today-entries)
-
-(global-unset-key [(f4)])
-(defun open-inbox()
-  (interactive)
-  (org-agenda "" "i"))
-(global-set-key [(f4)] 'open-inbox)
-
-(global-unset-key [(f5)])
-(defun open-review()
-  (interactive)
-  (org-agenda "" "r"))
-(global-set-key [(f5)] 'open-review)
-
-(global-unset-key [(f6)])
-(defun open-working-memory()
-  (interactive)
-  (find-file "~/Dropbox/wm.md")
-  (delete-blank-lines)
-  (goto-char (point-max))
-  (insert "\n")
-  (insert-date-time)
-  (insert "\n"))
-(global-set-key [(f6)] 'open-working-memory)
 
 (defun capture-log()
   (interactive)
   (org-capture nil "g"))
 (global-unset-key [(f7)])
 (global-set-key [(f7)] 'capture-log)
-
 
 (global-unset-key [(f10)])
 (global-set-key [(f10)] 'kmacro-start-macro)
@@ -75,7 +40,6 @@
 		  (set-face-attribute 'variable-pitch nil :height (- (face-attribute 'variable-pitch :height) 10))
 		  (set-face-attribute 'fixed-pitch nil :height (- (face-attribute 'fixed-pitch :height) 10))))
 
-
 (global-unset-key "\M-g")
 (global-set-key "\M-g" 'goto-line)
 ;;;;;;;;;;;;;;;
@@ -86,12 +50,6 @@
 (global-set-key "\C-cl" 'org-store-link)
 
 (global-set-key "%" 'match-paren)
-
-(global-unset-key [(control x) (control o)])
-;;* key binding
-(global-set-key [(control x) (control o)] 'grep-ffap)
-;;* end
-
     
 (global-unset-key (kbd "C-x C-b"))
 (global-set-key (kbd "C-x C-b") 'ibuffer)
